c38d2955a11a66d68e6968444438fb9b24188cea
kaveend <jayjayasinghe9@gmail.com>
2023-06-24 12:56:25 -0400

    Inital Commit

---
+++ app.js
@@ -0,0 +1,59 @@
+const express = require("express")
+const path = require("path")
+
+
+
+const app = express()
+
+var favicon = require('serve-favicon')
+
+app.use(favicon(__dirname + '/favicon.ico'));
+app.use('/images', express.static('assets/imgs'))
+app.use('/homepage.js', express.static('assets/js/homepage.js'))
+app.use('/helsinki.woff', express.static('assets/fonts/helsinki_regular_macroman/helsinki-webfont.woff'))
+app.use('/mikado', express.static('assets/fonts/mikado'))
+
+app.use(
+  "/css",
+  express.static(path.join(__dirname, "node_modules/bootstrap/dist/css"))
+)
+app.use(
+  "/css",
+  express.static(path.join(__dirname, "node_modules/bootstrap-icons/font/"))
+)
+app.use(
+  "/css",
+  express.static(path.join(__dirname, "assets/css"))
+)
+app.use(
+  "/js",
+  express.static(path.join(__dirname, "node_modules/bootstrap/dist/js"))
+)
+app.use(
+  "/js",
+  express.static(path.join(__dirname, "node_modules/animejs/lib"))
+)
+app.use("/js", express.static(path.join(__dirname, "node_modules/jquery/dist")))
+  
+
+
+app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, "views/index.html"))
+})
+
+app.get("/about", (req, res) => {
+  res.sendFile(path.join(__dirname, "views/about.html"))
+})
+
+app.get("/social_media", (req, res) => {
+  res.sendFile(path.join(__dirname, "views/socials.html"))
+})
+
+app.get("/equipment", (req, res) => {
+  res.sendFile(path.join(__dirname, "views/equipment.html"))
+})
+
+
+app.listen(3000, () => {
+    console.log(`Server is listening on port 3000`)
+})
---
+++ assets/js/homepage.js
@@ -0,0 +1,17 @@
+
+
+var textWrapper = document.querySelector('.home.header');
+textWrapper.innerHTML = textWrapper.textContent.replace(/\S/g, "<span class='letter'>$&</span>");
+
+
+anime.timeline()
+  .add({
+    targets: '.header .letter',
+    opacity: [0,1],
+    scale: [0.85, 1],
+    skewX: [5, 0],
+    filter: blur[13, 0],
+    easing: "cubicBezier(0.65,0.06,0.37,0.93)",
+    duration: 800,
+    delay: anime.stagger(100)
+  });
